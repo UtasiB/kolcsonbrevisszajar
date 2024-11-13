ae6bc0048bd6ead6069d9db365144a204c659617
bartekaron <78853724+bartekaron@users.noreply.github.com>
2024-11-13 12:26:16 +0100

    Add files via upload

--- kolcsonbre.sql
+++ kolcsonbre.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Gép: 127.0.0.1
--- Létrehozás ideje: 2024. Nov 06. 09:35
+-- Létrehozás ideje: 2024. Nov 13. 12:24
 -- Kiszolgáló verziója: 10.4.32-MariaDB
 -- PHP verzió: 8.2.12
 
@@ -34,6 +34,22 @@ CREATE TABLE `items` (
   `available` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_hungarian_ci;
 
+--
+-- A tábla adatainak kiíratása `items`
+--
+
+INSERT INTO `items` (`ID`, `title`, `type`, `available`) VALUES
+(1, 'Jani kalandjai ', 'könyv', 0),
+(2, 'Balázs kalandjai', 'film', 1),
+(3, 'Csoki kalandjai', 'könyv', 1),
+(4, 'Áron kalandjai', 'film', 1),
+(5, 'Kernel kalandjai', 'könyv', 0),
+(6, 'Tari kalandjai', 'film', 1),
+(7, 'Bezdan kalandjai', 'film', 0),
+(13, 'Dominik kalandjai', 'könyv', 1),
+(14, 'Máté kalandjai', 'film', 1),
+(15, 'Zsolti kalandjai', 'könyv', 1);
+
 -- --------------------------------------------------------
 
 --
@@ -48,10 +64,51 @@ CREATE TABLE `rentals` (
   `return_date` date DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_hungarian_ci;
 
+--
+-- A tábla adatainak kiíratása `rentals`
+--
+
+INSERT INTO `rentals` (`ID`, `userID`, `itemID`, `rental_date`, `return_date`) VALUES
+(8, 1, 3, '2024-11-11', '2024-11-11'),
+(9, 1, 6, '2024-11-11', '2024-11-11'),
+(10, 1, 3, '2024-11-11', '2024-11-11'),
+(11, 1, 5, '2024-11-11', '2024-11-11'),
+(16, 1, 1, '2024-11-11', '2024-11-11'),
+(17, 1, 4, '2024-11-11', '2024-11-11'),
+(18, 1, 3, '2024-11-11', '2024-11-11'),
+(19, 1, 4, '2024-11-11', '2024-11-11'),
+(20, 1, 2, '2024-11-11', '2024-11-11'),
+(21, 1, 1, '2024-11-11', '2024-11-11'),
+(22, 1, 2, '2024-11-11', '2024-11-11'),
+(23, 1, 4, '2024-11-11', '2024-11-11'),
+(24, 1, 5, '2024-11-11', '2024-11-11'),
+(25, 1, 3, '2024-11-11', '2024-11-11'),
+(26, 1, 1, '2024-11-11', '2024-11-11'),
+(27, 1, 1, '2024-11-11', '2024-11-11'),
+(28, 1, 1, '2024-11-11', '2024-11-11'),
+(29, 1, 1, '2024-11-11', '2024-11-11'),
+(30, 1, 7, '2024-11-12', '2024-11-12'),
+(31, 1, 6, '2024-11-12', '2024-11-12'),
+(32, 1, 3, '2024-11-12', '2024-11-12'),
+(33, 1, 6, '2024-11-12', '2024-11-12'),
+(34, 5, 2, '2024-11-12', '2024-11-12'),
+(36, 1, 1, '2024-11-12', '2024-11-12'),
+(37, 1, 5, '2024-11-12', '2024-11-12'),
+(38, 1, 5, '2024-11-12', '2024-11-12'),
+(40, 1, 1, '2024-11-13', '2024-11-13'),
+(41, 1, 1, '2024-11-13', '2024-11-13'),
+(42, 1, 6, '2024-11-13', '2024-11-13'),
+(43, 5, 1, '2024-11-13', '2024-11-13'),
+(44, 5, 6, '2024-11-13', '2024-11-13'),
+(45, 1, 5, '2024-11-13', NULL),
+(46, 1, 7, '2024-11-13', NULL),
+(47, 6, 1, '2024-11-13', NULL),
+(48, 6, 13, '2024-11-13', '2024-11-13');
+
 -- --------------------------------------------------------
 
 --
--- Tábla szerkezet ehhez a táblához `user`
+-- Tábla szerkezet ehhez a táblához `users`
 --
 
 CREATE TABLE `users` (
@@ -64,12 +121,13 @@ CREATE TABLE `users` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_hungarian_ci;
 
 --
--- A tábla adatainak kiíratása `user`
+-- A tábla adatainak kiíratása `users`
 --
 
 INSERT INTO `users` (`ID`, `name`, `email`, `passwd`, `membership_date`, `role`) VALUES
 (1, 'Ári', 'ari@turr.hu', '29b904ffa51141df00d6afedd88f2f18936511e2', '2024-11-05', 'admin'),
-(2, 'Bali', 'bali@turr.hu', '342f5bba34b5cc82ecc06d9b059ad57bd0dac5cd', '2024-11-05', 'admin');
+(5, 'jani', 'jani@turr.hu', '1798a15d09fd38eaaa10af3e06cd39c98c484501', '2024-11-11', 'user'),
+(6, 'Bazsz', 'bazsi@turr.hu', 'a9fc680b7ea715d40d541f3327a5b983612f248a', '2024-11-13', 'user');
 
 --
 -- Indexek a kiírt táblákhoz
@@ -91,7 +149,7 @@ ALTER TABLE `rentals`
   ADD KEY `itemID` (`itemID`);
 
 --
--- A tábla indexei `user`
+-- A tábla indexei `users`
 --
 ALTER TABLE `users`
   ADD PRIMARY KEY (`ID`);
@@ -104,19 +162,19 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT a táblához `items`
 --
 ALTER TABLE `items`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
 -- AUTO_INCREMENT a táblához `rentals`
 --
 ALTER TABLE `rentals`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=49;
 
 --
--- AUTO_INCREMENT a táblához `user`
+-- AUTO_INCREMENT a táblához `users`
 --
 ALTER TABLE `users`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- Megkötések a kiírt táblákhoz
